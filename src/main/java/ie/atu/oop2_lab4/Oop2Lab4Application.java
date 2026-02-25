376b1d8489e48de4d0f63b44cad90b2c0a8aabdc
DenysKapustianskyi-S <G00470377@atu.ie>
2026-02-25 16:48:23 +0000

    initial commit

---
+++ src/main/java/ie/atu/oop2_lab4/Oop2Lab4Application.java
@@ -0,0 +1,13 @@
+package ie.atu.oop2_lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oop2Lab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oop2Lab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/oop2_lab4/Oop2Lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.oop2_lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oop2Lab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
